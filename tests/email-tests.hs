4f608bbc564c6e3f7e86a27b082bf3996e1ee926
Melanie Brown <brown.m@pm.me>
2023-06-19 19:06:23 -0400

    no relude on tests

--- tests/email-tests.hs
+++ tests/email-tests.hs
@@ -1,28 +1,25 @@
 module Main (main) where
 
 import Control.Monad.Email
+import Data.Function ((&))
 import Test.Hspec
 import Text.Hamlet (shamlet)
 
 main :: IO ()
 main = putStrLn "test suite not implemented yet"
 
-melbox :: Mailbox
-melbox = [mailbox|Melanie Brown <brown.m@pm.me>|]
+dummyEmail :: Mailbox
+dummyEmail = [mailbox|Dummy Email <dummy@email.ftw>|]
 
 testmsg :: Mail
 testmsg =
-  newmessage melbox
-    & to [melbox]
+  newmessage dummyEmail
+    & to [dummyEmail]
     & subject "Testing out package smtp-hs"
     & body
       [shamlet|
-          <html>
-            <style>
-              .green {
-                background-color: #009fff;
-              }
-            <body>
-              <p>Test content.
-              <p .green>Please ignore.
+        <html>
+          <body>
+            <p>Test content.
+            <p style="background-color: #009fff;">Please ignore.
       |]
